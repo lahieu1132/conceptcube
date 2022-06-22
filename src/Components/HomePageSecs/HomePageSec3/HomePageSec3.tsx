120a7ec14affdfd8825fc2680ec42548f62f342f
Hieu <lahieu240@gmail.com>
2022-06-22 16:27:46 +0700

    up3

--- src/Components/HomePageSecs/HomePageSec3/HomePageSec3.tsx
+++ src/Components/HomePageSecs/HomePageSec3/HomePageSec3.tsx
@@ -7,6 +7,10 @@ import img2 from "../../../Assets/Images/Page3/off-game-mar-img@2x.png"
 import img3 from "../../../Assets/Images/Page3/off-si-img@2x.png"
 import img4 from "../../../Assets/Images/Page3/off-ui-design-img@2x.png"
 
+import img1a from "../../../Assets/Images/Page3/on-game-dev-img@2x.png"
+import img2a from "../../../Assets/Images/Page3/on-game-mar-img@2x.png"
+import img3a from "../../../Assets/Images/Page3/on-si-img@2x.png"
+import img4a from "../../../Assets/Images/Page3/on-ui-design-img@2x.png"
 
 
 
@@ -14,21 +18,25 @@ const items= [
   {
     title: 'Game Development',
     img: img1,
+    imgActive: img1a,
     desc: "VFX, 소셜광고 디자인, 아트웍, 게임 개발 등 이미지와 디자인 제작에 필요한 모든 것 그리고 고객이 필요로 하는 기술과 결과물을 제공합니다. 또한 모션그래픽 및 자막, 종적 타이포그래피, 애니메이션 등 촬영 이미지와 CG를 합성하여 완성도 있는 결과물을 도출합니다."
   },
   {
     title: 'UI design',
     img: img2,
+    imgActive: img2a,
     desc: "VFX, 소셜광고 디자인, 아트웍, 게임 개발 등 이미지와 디자인 제작에 필요한 모든 것 그리고 고객이 필요로 하는 기술과 결과물을 제공합니다. 또한 모션그래픽 및 자막, 종적 타이포그래피, 애니메이션 등 촬영 이미지와 CG를 합성하여 완성도 있는 결과물을 도출합니다."
   },
   {
     title: 'SI',
     img: img3,
+    imgActive: img3a,
     desc: "VFX, 소셜광고 디자인, 아트웍, 게임 개발 등 이미지와 디자인 제작에 필요한 모든 것 그리고 고객이 필요로 하는 기술과 결과물을 제공합니다. 또한 모션그래픽 및 자막, 종적 타이포그래피, 애니메이션 등 촬영 이미지와 CG를 합성하여 완성도 있는 결과물을 도출합니다."
   },
   {
     title: 'Game Marketing',
     img: img4,
+    imgActive: img4a,
     desc: "VFX, 소셜광고 디자인, 아트웍, 게임 개발 등 이미지와 디자인 제작에 필요한 모든 것 그리고 고객이 필요로 하는 기술과 결과물을 제공합니다. 또한 모션그래픽 및 자막, 종적 타이포그래피, 애니메이션 등 촬영 이미지와 CG를 합성하여 완성도 있는 결과물을 도출합니다."
   },
 ]
@@ -36,19 +44,21 @@ const items= [
 function HomePageSec3() {
   const [itemsLeft, setItemsLeft] = useState([])
   const [itemsRight, setItemsRight] = useState([])
-  
+  const [itemOver, setItemOver] = useState<number|null>(null)
 
   function onMouseOver(event: MouseEvent, index: number){
     const itemListLeft: any = items.filter((item, i:number) => i < index).map((item, i:number) => i)
     setItemsLeft(itemListLeft)
     const itemListRight: any = items.filter((item, i:number) => i > index).map((item, i:number) => (index + i + 1))
     setItemsRight(itemListRight)
+    setItemOver(index)
 
   }
 
   const onMouseOut = (event: MouseEvent, index: number) => {
     setItemsRight([])
     setItemsLeft([])
+    setItemOver(null)
   }
 
 
@@ -60,7 +70,7 @@ function HomePageSec3() {
             <div className="home-three-items ">
               {
                 items.map((item, index) : any => {
-                  const { title, desc, img} = item
+                  const { title, desc, img, imgActive} = item
                   return (
                     <div className={`home-three-item select-none ${itemsLeft.some(item => item == index) && "left"} ${itemsRight.some(item => item == index) && "right"}`} key={index}
                       onMouseOver={(event) => onMouseOver(event, index)}
@@ -74,7 +84,7 @@ function HomePageSec3() {
                         ))}
                       </h3>
                       <div className="home-three-item-img">
-                        <img src={img} alt="" />
+                        <img src={itemOver == index ? imgActive : img} alt="" />
                       </div>
                       <div className="home-three-item-desc ">
                         <h4>{title}</h4>
